770de348734c6f11a1c827bdba19bc9fd501f5f7
주환 <paullee0526@gmail.com>
2026-02-09 22:40:03 +0900

    업데이트 - 앱 테마 선택

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jsjh_todaily.test_todaily_ver1"
         minSdk = 28
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.7"
+        versionCode = 10
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
